4d266b0b2159fb5c56cde8ec1355fb54359a0955
Lacina <luky.lacina@gmial.com>
2024-12-01 15:29:12 +0100

    rozvržení struktury algoritmu pro RO

--- Program/model.py
+++ Program/model.py
@@ -1,9 +1,21 @@
 import tensorflow
 
-def create_face_recognition_model(input_shape=(128, 128,3 )):
+def create_face_recognition_model(input_shape=(128, 128, 3)):
     """
     Vytvoření vlastního modelu pro RO pomocí CNN
+
+    Modul extrahuje rysy obličeje a poté je převede do embeddingu (do matematické reprezentace vektorových vzdáleností), která slouží k rychlému porovnávání a identifikaci obličejů
 
     Příchozí data:
         parametr: input_shape - určuje tvar vstupních dat (velikost 128x128, má 3 kanály = tnz. RGB formát)
     """
+    model = tensorflow.keras.models.Sequential() # Využívá se sekvenční model (vrstvy jsou přidávány v lineárním pořadí - každá výstup vrstvy je vstupem do další vrsty)
+
+    # První konvolunční vrstva
+
+    # Druhá konvolunční vrstva
+
+    # Třetí konvolunční vrstva
+
+    # Vrstva pro vygenerování embeddingu
+
