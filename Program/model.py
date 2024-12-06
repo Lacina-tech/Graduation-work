dc60f31769b056ceae93b9d135c5c12487aedd3e
Lacina <luky.lacina@gmial.com>
2024-12-06 20:52:46 +0100

    první pokusy trénování modelu RO

--- Program/model.py
+++ Program/model.py
@@ -3,9 +3,12 @@
 
 # Import knihovny
 import tensorflow
+import numpy as np
+import os
+import cv2
 
 # Vytvoření vlastní struktury algoritmu pro RO pomocí tensorflow
-def create_face_recognition_model(input_shape=(128, 128, 3)):
+def create_face_recognition_model(input_shape=(128, 128, 3), num_classes=None):
     """
     Vytvoření vlastního modelu pro RO pomocí CNN
 
@@ -44,26 +47,67 @@ def create_face_recognition_model(input_shape=(128, 128, 3)):
     # Tato vrstva zpracovává informace z předchozích vrstev a vytváří komplexní reprezentaci obličeje (Dense)
         # 256 - množství neuronů, které vrstva obsahuje
     model.add(tensorflow.keras.layers.Dense(256, activation="relu"))
-    # Tato vrstva dotváří finální obraz obličeje pomocí vektorů, využívá 128 neuronů
-    model.add(tensorflow.keras.layers.Dense(128, activation="relu"))
+    # Tato vrstva dotváří finální obraz obličeje pomocí vektorů, využívá 128 neuronů 
+  
+    # Výstupní vrstva pro klasifikaci
+    if num_classes:
+        model.add(tensorflow.keras.layers.Dense(num_classes, activation="softmax"))
+    else:
+        model.add(tensorflow.keras.layers.Dense(128, activation="relu"))  # Embedding pro porovnání obličejů
 
-    # Normalizace embeddingu (pro porovnávání pomocí vektorových vzdáleností)
-    model.add(tensorflow.keras.layers.Lambda(lambda x: tensorflow.math.l2_normalize(x, axis=1)))
-
-    # Kompilace modelu
+    # Kompilace modelu s categorical crossentropy pro více tříd
     model.compile(
-        optimizer=tensorflow.keras.optimizers.Adam(learning_rate=0.001), # Optimalizátor Adam, který optimalizuje model
-        loss=tensorflow.keras.losses.CosineSimilarity(axis=1),  # Ztrátová funkce, které pracuje s orientací vektorů embeddingu, využívá Kosinovou podobnost (zanedbává velikost vektorů)
-        metrics=[tensorflow.keras.metrics.CosineSimilarity(axis=1, name="cosine_similarity")]  # Metrika, která sleduje přesnost porovnání během trénování a testování
+        optimizer=tensorflow.keras.optimizers.Adam(learning_rate=0.001),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
     )
 
     # Vrácení modelu
     return model
 
+# Funkce pro načtení a přípravu dat
+def load_data(dataset_directory):
+    data = []
+    labels = []
+    
+    for label in os.listdir(dataset_directory):
+        label_path = os.path.join(dataset_directory, label)
+        for photo_name in os.listdir(label_path):
+            photo_path = os.path.join(label_path, photo_name)
+            # Načtení obrázku
+            image = cv2.imread(photo_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Převod na RGB
+            image = cv2.resize(image, (128, 128))  # Změna velikosti
+            image = image.astype("float32") / 255.0  # Normalizace (Bude odstraněna - použita jinde)
+
+            data.append(image)
+            labels.append(int(label))
+
+    # Vrácí data a příslučné labely
+    return np.array(data), np.array(labels)
+
+# Načtení dat
+train_data, train_labels = load_data(r"Program\dataset\preprocessed dataset.1 (LFW)\train")
+test_data, test_labels = load_data(r"Program\dataset\preprocessed dataset.1 (LFW)\test")
+
+# Převod štítků na one-hot encoding
+num_classes = len(os.listdir(r"Program\dataset\preprocessed dataset.1 (LFW)\train"))
+train_labels = tensorflow.keras.utils.to_categorical(train_labels, num_classes)
+test_labels = tensorflow.keras.utils.to_categorical(test_labels, num_classes)
+
 # Vytvoření modelu
-model = create_face_recognition_model()
+model = create_face_recognition_model(input_shape=(128, 128, 3), num_classes=num_classes)
 
 # Trénování modelu
+history = model.fit(
+    train_data,
+    train_labels,
+    validation_data=(test_data, test_labels),
+    epochs=25,
+    batch_size=32,
+    shuffle=True
+)
 
-# Uložení modelu ve formátu H5
+# Uložení modelu
 model.save("face_recognition_model.h5")
+print("Model byl úspěšně uložen jako 'face_recognition_model.h5'.")
